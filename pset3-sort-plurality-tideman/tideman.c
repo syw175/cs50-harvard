5bd23730a6891f36cfc429547defafb294dc9fe4
Steven Wong <syw14@sfu.ca>
2022-06-29 19:48:45 -0700

    complete vote function

--- pset3-sort-plurality-tideman/tideman.c
+++ pset3-sort-plurality-tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -99,7 +100,12 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++) {
+        if (strcmp(name, candidates[i]) == 0) {
+            ranks[rank] = i;
+            return true;
+        }
+    }
     return false;
 }
 
