cdfa54ed8327f5bb225f1129ec3ca5e4b5219d47
Steven Wong <syw14@sfu.ca>
2022-07-05 17:25:44 -0700

    image reflection func

--- pset4-volume-filter-recover/filter-more/helpers.c
+++ pset4-volume-filter-recover/filter-more/helpers.c
@@ -1,6 +1,7 @@
 #include "helpers.h"
 #include <math.h>
 
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -8,9 +9,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int col = 0; col < width; col++) 
         {
+            // Get a pointer to the current pixel and the calculate the required gray scale color
             RGBTRIPLE *currentColor = &image[row][col];
-            BYTE grayScaleCol = round((currentColor->rgbtRed + currentColor->rgbtBlue + currentColor->rgbtGreen)/3.0);
+            BYTE grayScaleCol = round((currentColor->rgbtRed + currentColor->rgbtBlue
+                                         + currentColor->rgbtGreen)/3.0);
 
+            // Assign the gray scale color to the RGB values of the current pixel
             currentColor->rgbtRed = grayScaleCol;
             currentColor->rgbtBlue = grayScaleCol;
             currentColor->rgbtGreen = grayScaleCol;
@@ -23,16 +27,44 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0, n = width/2; col < n; col++)
+        {
+            // Swap current pixel with the next rightmost pixel
+            RGBTRIPLE colorBuffer = image[row][col];
+            image[row][col] = image[row][width-col-1];
+            image[row][width-col-1] = colorBuffer;
+
+            // Note: Pointers unnecessary here because C passes the address of the first value from image
+            // RGBTRIPLE *currentColor = &image[row][col];
+            // RGBTRIPLE *toBeReflected = &image[row][width-col-1];
+
+            // RGBTRIPLE colorBuffer = image[row][col];
+            // *currentColor = *toBeReflected;
+            // *toBeReflected = colorBuffer;
+        }
+    }
     return;
 }
 
-// Blur image
+// Blur image - TODO
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE adjPixels[6];
+    int pixelCount = 0;
+
+    for (int row = 0; row < height; row++) { 
+        for (int col = 0; col < width; col++) { 
+
+        }
+    }
+
+
     return;
 }
 
-// Detect edges
+// Detect edges - TODO
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
