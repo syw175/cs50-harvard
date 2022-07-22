9a565e1c63e9e1b2aa40255f6e9f15c297097a29
Steven Wong <syw14@sfu.ca>
2022-07-21 18:03:08 -0700

    main function

--- pset6-worldcup-readability-dna/sentimental-readability/readability.py
+++ pset6-worldcup-readability-dna/sentimental-readability/readability.py
@@ -1,1 +1,33 @@
-# TODO
+
+
+
+def main(): 
+
+    letterCount = 0;
+    wordCount = 0; 
+    sentenceCount = 0;
+
+    gradeIndex = getColemanIndex(letterCount, wordCount, sentenceCount)
+
+    # Print the grade level
+    if (gradeIndex >= 16):
+        print("Grade 16+");
+
+    elif (gradeIndex > 1):
+        print(f"Grade {gradeIndex}")
+
+    else:
+        print("Before Grade 1")
+    
+
+
+def getColemanIndex(letterCount, wordCount, sentenceCount): 
+    return
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
