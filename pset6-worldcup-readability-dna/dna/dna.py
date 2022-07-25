c78fa1648779b59f5227ec4d387feb7746be9d81
Steven Wong <syw14@sfu.ca>
2022-07-25 13:36:09 -0700

    clean up code and refactor

--- pset6-worldcup-readability-dna/dna/dna.py
+++ pset6-worldcup-readability-dna/dna/dna.py
@@ -7,59 +7,57 @@ from numpy import true_divide
 
 def main():
 
-    # Check for command-line usage
+    # Check for correct command-line usage, if not, exit
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         sys.exit()
 
+
     # Read database file into a variable
-    # {'Alice': {'AGATC': '2', 'AATG': '8', 'TATC': '3'}, 'Bob': {'AGATC': '4', 'AATG': '1', 'TATC': '5'}, 'Charlie': {'AGATC': '3', 'AATG': '2', 'TATC': '5'}}
-    database = {}
-    with open(sys.argv[1], "r") as file:
-        fileReader = csv.DictReader(file)
-        for row in fileReader: 
-            name = row["name"]
-            row.pop("name")
-            database[name] = row
-        # temp name
-        possibleSeq = list(row.keys())
-    
+    # {'Alice': {'AGATC': '2', 'AATG': '8', 'TATC': '3'}, 'Bob': {'AGATC': '4', 'AATG': '1', 'TATC': '5'}... }
+    dnaDatabase = {}
+    with open(sys.argv[1], 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        # For each row in the database file, add the name and DNA sequence to the dictionary
+        for row in reader:
+            dnaDatabase[row['name']] = row
+            # Remove the name key:pair from the dictionary values
+            dnaDatabase[row['name']].pop('name')
+
+
+    # Get list of all sequences in the database
+    # ['AGATC', 'AATG', 'TATC', 'AGATC', 'AATG', 'TATC', 'AGATC', 'AATG', 'TATC']
+    dnaSequences = []
+    for name in dnaDatabase:
+        # For each name, add the DNA sequence to the list
+        for sequence in dnaDatabase[name]:
+            dnaSequences.append(sequence)
+
+
     # Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as file: 
         dnaSequence = file.read()
 
-    # TODO: Find longest match of each STR in DNA sequence
-    # print(possibleSeq)
 
-    longestMatch = {} 
-    for sequence in possibleSeq: 
-        longestMatch[sequence] = longest_match(dnaSequence, sequence)
+    # Find longest match of each STR in DNA sequence
+    # {'AGATC': '4', 'AATG': '1', 'TATC': '5'}
+    dnaSequenceMatches = {}
+    for STR in dnaSequences:
+        dnaSequenceMatches[STR] = str(longest_match(dnaSequence, STR))
     
-    # print(longestMatch)
-    
-    
-    printed = False
-    # TODO: Check database for matching profiles
-    for person in database: 
-        flag = True
-        
-        current = database[person]
-        for sequence in current: 
-            if longestMatch[sequence] != int(current[sequence]): 
-                flag = False
-        
-        if flag == True:
+
+    # Check database for matching profiles
+    for person in dnaDatabase: 
+        # If the DNA sequence matches the database, print the name and exit
+        if dnaDatabase[person] == dnaSequenceMatches:
             print(person)
+            sys.exit()
 
 
-
-    if not printed: 
-        print("No match.")
-
-
-    
-
+    # At this point, no match has been found in the database
+    print("No match.")
     return
+
 
 def longest_match(sequence, subsequence):
     """Returns length of longest run of subsequence in sequence."""
